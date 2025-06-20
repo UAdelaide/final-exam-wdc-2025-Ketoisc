bda2bea1036758b2992ce52c84c0480edaa9202c
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 07:54:12 +0000

    [automatic commit] 2025-06-20 07:54:12 UTC 192.168.20.125

--- part2/app.js
+++ part2/app.js
@@ -6,13 +6,19 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(session({
+    secret: 'skibidi',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}))
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 const session = require('express-session');
-
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
