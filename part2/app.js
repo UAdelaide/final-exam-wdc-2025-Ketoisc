03348a8f2aeddc7393b27e6784652979dc23a051
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 07:55:58 +0000

    [automatic commit] 2025-06-20 07:55:58 UTC 192.168.20.125

--- part2/app.js
+++ part2/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 var mysql = require('mysql2/promise');
+const session = require('express-session');
 const app = express();
 
 // Middleware
