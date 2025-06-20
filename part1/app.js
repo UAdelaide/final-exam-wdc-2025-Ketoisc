067b6f80ab93753ac58e05a58821157f710d1982
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 03:45:10 +0000

    [automatic commit] 2025-06-20 03:45:10 UTC 192.168.20.125

--- part1/app.js
+++ part1/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mysql = require('mysql2/promise');
+
 var app = express();
 
 app.use(logger('dev'));
