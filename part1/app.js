e26cc0743e47703bd01becd943b4c9b5a1f54ca2
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 03:13:49 +0000

    [automatic commit] 2025-06-20 03:13:49 UTC 192.168.20.125

--- part1/app.js
+++ part1/app.js
@@ -126,7 +126,7 @@ app.use('/users', usersRouter);
   // Route to return all dogs as JSON with size and owner's username
   app.get('/api/dogs', async (req, res) => {
     try {
-      const [books] = await db.execute('SELECT dog_name, size, dogs.name AS  FROM books');
+      const [books] = await db.execute('SELECT dog_name, size, users.username AS owner_username FROM books');
       res.json(books);
     } catch (err) {
       res.status(500).json({ error: 'Failed to fetch books' });
