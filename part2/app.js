1b52a3d0a0d050476da1a28e4cead1da2cd050c3
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 09:40:54 +0000

    [automatic commit] 2025-06-20 09:40:54 UTC 192.168.20.125

--- part2/app.js
+++ part2/app.js
@@ -18,10 +18,12 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const dogRoutes = require('./routes/dogRoutes');
 
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/dogs', userRoutes);
 
 
 
