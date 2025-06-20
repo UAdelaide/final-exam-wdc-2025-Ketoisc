67ddddec82df298858a535bc97ea9fa4716312db
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 03:16:01 +0000

    [automatic commit] 2025-06-20 03:16:01 UTC 192.168.20.125

--- part1/app.js
+++ part1/app.js
@@ -134,7 +134,7 @@ app.use('/users', usersRouter);
   });
 
   // Route to return all dogs as JSON with size and owner's username
-  app.get('/api/dogs', async (req, res) => {
+  app.get('/api/walkrequests/open', async (req, res) => {
     try {
       const [books] = await db.execute('SELECT dogs.dog_name, dogs.size, users.username AS owner_username FROM Dogs dogs JOIN Users users ON dogs.owner_id = users.user_id');
       res.json(books);
