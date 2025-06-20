2ed18bae7ba16a768faaea2dbe3db94655bba027
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 03:35:33 +0000

    [automatic commit] 2025-06-20 03:35:32 UTC 192.168.20.125

--- part1/app.js
+++ part1/app.js
@@ -146,7 +146,7 @@ app.use('/users', usersRouter);
 // Route to return summary of each walker as JSON
   app.get('/api/walkers/summary', async (req, res) => {
     try {
-      const [books] = await db.execute("SELECT user.username AS walker_username, (SELECT COUNT(*) FROM WalkRatings) AS total_ratings, ");
+      const [books] = await db.execute("SELECT user.username AS walker_username, (SELECT COUNT(*) FROM WalkRatings) AS total_ratings, SELECT AVG(");
       res.json(books);
     } catch (err) {
       res.status(500).json({ error: 'Failed to fetch dog list' });
