e2a07a5ad86f24a0e6c7618051f101b648df8d20
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 08:12:01 +0000

    [automatic commit] 2025-06-20 08:12:01 UTC ;; communications error to 208.67.222.222#53: timed out 192.168.20.125

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,7 +62,7 @@ router.post('/login', async (req, res) => {
 });
 
 // POST logout
-router.post('/login', async (req, res) => {
+router.post('/logout', async (req, res) => {
     req.session.destroy(err => {
       if (err) {
         return res.status(500).json({ error: 'Failed logout' });
