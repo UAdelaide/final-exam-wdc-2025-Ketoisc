517f2c3fef0e50e27e2af14f13b5ed4efb306a7a
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-06-20 03:00:09 +0000

    [automatic commit] 2025-06-20 03:00:09 UTC 192.168.20.125

--- part1/app.js
+++ part1/app.js
@@ -97,11 +97,12 @@ app.use('/users', usersRouter);
 
 
       // Insert data if table is empty
-      const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+      const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
       if (rows[0].count === 0) {
         await db.execute(`
         INSERT INTO Users (username, email, password_hash, role)
         VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'), ('bobwalker', 'bob@example.com', 'hashed456', 'walker'), ('carol123', 'carol@example.com', 'hashed789', 'owner'), ('brooke3', 'brooke@example.com', 'password123', 'owner'), ('irene45', 'irene@example.com', 'pass123', 'walker');
+        `
         INSERT INTO Dogs (owner_id, name, size)
         VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'), ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'), ((SELECT user_id FROM Users WHERE username = 'brooke3'), 'Kai', 'large'), ((SELECT user_id FROM Users WHERE username = 'brooke3'), 'Martin', 'small'), ((SELECT user_id FROM Users WHERE username = 'brooke3'), 'Grace', 'medium');
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
